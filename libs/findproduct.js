b7031ca2ef87997f6971e9a89a430fc3ee1227b1
bombpersons <blindrabbits@gmail.com>
2016-01-13 22:39:18 +0000

    Now using CasperJS. WIP findproduct.js

--- libs/findproduct.js
+++ libs/findproduct.js
@@ -1,39 +1,167 @@
+var require = patchRequire(require);
+
 // Get's a random product from a page of items.
-exports.findproduct = function(url) {
-  // Open up a page...
-  var page = require('webpage').create();
-  page.onConsoleMessage = function(msg) { console.log(msg); };
+exports.findproduct = function(search, currency, lowbudget, highbudget) {
+  var totalResultCount = 0;
+  var itemsPerPageCount = 0;
+  var totalPages = 0;
 
-  page.onError = function (msg, trace) {
-      console.log(msg);
-      trace.forEach(function(item) {
-          console.log('  ', item.file, ':', item.line);
-      });
-  };
+  var randomNum = 0;
+  var randomPage = 0;
 
-  console.log('Opening page: ' + url);
-  page.open(url, function(status) {
-    console.log('Status: ' + status);
-    if (status === 'success') {
-      console.log('hey');
-
-      // Get the total page count.
-      var totalResultCount = page.evaluate(function() {
-        var countTag = document.getElementsByClassName('search-count');
-        return countTag[0].innerHTML;
-      });
-      // Get rid of commas
-      totalResultCount = totalResultCount.replace(',', '');
-
-      // Get the amount of items per page
-      var itemsPerPageCount = page.evaluate(function() {
-        var resultListTags = document.getElementsByClassName('list-item');
-        return resultListTags.length;
-      });
-
-      console.log('Results: ' + totalResultCount);
-      console.log('Results per page: ' + itemsPerPageCount);
-      console.log('Total pages: ' + totalResultCount / itemsPerPageCount);
-    }
+  var casper = require('casper').create();
+  casper.on('remote.message', function(msg) {
+    console.log('remote: ' + msg);
   });
+
+
+  casper.start('http://aliexpress.com/af/' + search + '.html');
+
+  // Narrow the search to match our budget.
+  casper.then(function() {
+    console.log('Searching for "' + search + '" with prices between ' + lowbudget + currency + ' and ' + highbudget + currency + '.');
+    if (!this.exists('form#narrow-form')) {
+      console.log("No results!");
+      this.die();
+    }
+
+    this.fill('form#narrow-form', {
+      'SearchText': search,
+      'minPrice': lowbudget,
+      'maxPrice': highbudget
+    }, true);
+
+  });
+
+  // Make sure the results are in the right currency.
+  casper.then(function() {
+    console.log('Clicking ' + currency + ' button.');
+    this.click('a[data-currency="' + currency + '"]');
+    this.wait(1000);
+
+    console.log('Clicking gallery view button.');
+    this.click('a[title="Gallery"]');
+    this.wait(1000);
+  });
+
+  // Pick one something at random.
+  casper.then(function() {
+    console.log('Getting total page count...');
+
+    // Get the total amount of items.
+    totalResultCount = this.evaluate(function () {
+      var countTag = document.getElementsByClassName('search-count');
+      return countTag[0].innerHTML;
+    });
+
+    // Get rid of commas
+    totalResultCount = totalResultCount.replace(',', '');
+
+    // Get how many items there are per page.
+    itemsPerPageCount = this.evaluate(function() {
+      var resultListTags = document.getElementsByClassName('product');
+      return resultListTags.length;
+    });
+
+    // We can calculate how many pages there are.
+    totalPages = totalResultCount / itemsPerPageCount;
+
+    console.log('Total Items: ' + totalResultCount);
+    console.log('Items per page: ' + itemsPerPageCount);
+    console.log('Total Pages: ' + totalPages);
+
+    // Seems the maximum page that you can view is 243...
+    // Not sure why...
+    var maxPage = 243;
+    if (totalPages > maxPage)
+      totalPages = maxPage;
+  });
+
+  // Pick an item on the page.
+  casper.then(function() {
+    function pickItem() {
+      var itemTags = document.getElementsByClassName('list-item');
+      var randomNum = Math.floor(Math.random() * itemTags.length);
+
+      //console.log('randomNum: ' + randomNum);
+      var itemTag = itemTags[randomNum];
+
+      // Get the price
+      var priceTag = itemTag.getElementsByClassName('price')[0];
+      //console.log('priceTag: ' + priceTag.innerHTML);
+
+      var price = priceTag.getElementsByClassName('value')[0].innerHTML;
+      price = price.replace(/[^1234567890.-]+/g, '');
+
+      // The price can be in the form of 10.00-15.00
+      // Just take the bigger one.
+      // TODO make this work if there is a price within our budget.
+      var prices = price.split('-');
+      if (prices.length == 2) {
+        price = prices[1];
+      }
+      price = Number(price);
+
+      //console.log('price: ' + price);
+
+      // Shipping price if there is one.
+      var shipping = 0;
+      var shippingTag = itemTag.getElementsByClassName('pnl-shipping')[0];
+      if (shippingTag) {
+        //console.log('shippingTag: ' + shippingTag.innerHTML);
+
+        shipping = shippingTag.getElementsByClassName('value')[0].innerHTML;
+        shipping = Number(shipping.replace(/[^1234567890.]+/g, ''));
+
+        //console.log('shipping: ' + shipping);
+      }
+
+      // The URL
+      var urlTag = itemTag.getElementsByClassName('product')[0];
+      var url = urlTag.getAttribute('href');
+      var name = urlTag.innerHTML;
+
+      //console.log('url: ' + url);
+
+      return {
+        'name': name,
+        'URL': url,
+        'price': price + shipping
+      };
+    }
+
+    // If there are any results..
+    if (totalResultCount > 0) {
+      var item = {};
+      while (true) {
+        // Generate a random page that is in range.
+        if (totalPages > 1) {
+          randomPage = Math.floor(Math.random() * totalPages);
+          console.log('Going to page ' + randomPage);
+          this.sendKeys('input#pagination-bottom-input', randomPage.toString(), {reset: true});
+          this.click('input#pagination-bottom-goto');
+
+          this.capture(randomPage + '.png');
+          console.log("waited.");
+        }
+
+        item = this.evaluate(pickItem);
+        if (item.price >= lowbudget && item.price <= highbudget) {
+          break;
+        } else {
+          console.log('Rejected: ' + item.URL + ' at total price of  ' + item.price);
+        }
+      }
+
+      console.log('Picked: ' + item.name + ' at total price of ' + item.price);
+      console.log(item.name);
+    } else {
+      // No results.
+      console.log("Couldn't find anything!");
+    }
+
+    });
+
+
+  casper.run();
 };
--- tests/findproduct.js
+++ tests/findproduct.js
@@ -1,4 +1,4 @@
 var findproduct = require('../libs/findproduct');
 
 // Grab some random search page.
-findproduct.findproduct('http://www.aliexpress.com/af/test.html?ltype=wholesale&SearchText=test&d=y&origin=n&initiative_id=SB_20160112152558&isViewCP=y&catId=0');
+findproduct.findproduct('Kansas', 'GBP', 5, 10);
